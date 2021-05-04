9bfb066cafcf5b1f514cf46f1b05c52039d26620
jalallll <jalalq11@gmail.com>
2021-05-04 19:47:26 -0400

    fixed button

--- src/Home.js
+++ src/Home.js
@@ -22,9 +22,6 @@ const Home = () => {
           <Col sm={true} className="pt-5 mt-5">
             <h2>Hello, I'm Jalal.</h2>
             <h2>I'm a Full Stack Software Developer</h2>
-            <button class="font-semibold bg-green-600 text-black-900 pt-2 pb-2 pr-3 pl-3 rounded-full hover:bg-green-400 ring-2 ">
-              Github
-            </button>
             <Link
               activeClass="active"
               to="projects"
@@ -33,30 +30,13 @@ const Home = () => {
               duration={500}
             >
               <button class="font-semibold bg-green-600 text-black-900 pt-2 pb-2 pr-3 pl-3 rounded-full hover:bg-green-400 ring-2 ">
-                go
+                Lets go on an adventure
               </button>
             </Link>
           </Col>
         </Row>
       </Container>
-      <Container className="" id="projects">
-        <Projects />
-        <Projects />
-
-        <Projects />
-
-        <Projects />
-
-        <Projects />
-
-        <Projects />
-
-        <Projects />
-
-        <Projects />
-
-        <Projects />
-      </Container>
+      <Container className="" id="projects"></Container>
     </div>
   );
 };
