aa865bcae6129c8e230fd0fef8c757c32a338330
jalallll <jalalq11@gmail.com>
2021-05-04 20:42:08 -0400

    gradient bg and fade in hero section

--- src/Home.js
+++ src/Home.js
@@ -12,31 +12,33 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div>
-      <NavBar />
-      <Container className="wrapper ">
-        <Row className="pt-5 hero">
-          <Col sm={true} className="text-center">
-            <img src={hello}></img>
-          </Col>
-          <Col sm={true} className="pt-5 mt-5">
-            <h2>Hello, I'm Jalal.</h2>
-            <h2>I'm a Full Stack Software Developer</h2>
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              offset={0}
-              duration={500}
-            >
-              <button class="font-semibold bg-green-600 text-black-900 pt-2 pb-2 pr-3 pl-3 rounded-full hover:bg-green-400 ring-2 ">
-                Lets go on an adventure
-              </button>
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="" id="projects"></Container>
+    <div className="home">
+      <div className="hero-section">
+        <NavBar />
+        <Container className="wrapper ">
+          <Row className="pt-5 hero">
+            <Col sm={true} className="text-center">
+              <img src={hello}></img>
+            </Col>
+            <Col sm={true} className="pt-5 mt-5">
+              <h2>Hello, I'm Jalal.</h2>
+              <h2>I'm a Full Stack Software Developer</h2>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                offset={0}
+                duration={500}
+              >
+                <button class="mt-3 font-semibold bg-green-600 text-black-900 pt-2 pb-2 pr-3 pl-3 rounded-full hover:bg-green-400 ring-2">
+                  Lets go on an adventure
+                </button>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+        <Container className="" id="projects"></Container>
+      </div>
     </div>
   );
 };
