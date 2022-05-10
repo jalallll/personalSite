ac9b7c4ec45e5a21eb583beb6379e46966b49832
jalallll <jalalq11@gmail.com>
2022-05-09 21:34:36 -0400

    new navbar responsive tailwind

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import { Box, Stack } from "@mui/material";
 import React from "react";
 import Projects from "./Components/Projects";
-import NavBar from "./Components/NavBar";
 import Hero from "./Components/Hero";
 import Skills from "./Components/Skills";
 import Card from "./Components/Card/Card";
 import Footer from "./Components/Footer";
+import Nav_bar from "./Components/Nav_bar";
 function App() {
 	return (
 		<Box bgcolor="gray">
 			<div>
-				<NavBar />
-				<Box sx={{ height: "100vh", alignItems: "center", display: "flex" }}>
-					<Hero />
-				</Box>
-				<Box sx={{ height: "100vh", alignItems: "center", display: "flex" }}>
-					<Skills />{" "}
-				</Box>
-				<Box sx={{ height: "100vh", alignItems: "center", display: "flex" }}>
-					<Projects />{" "}
-				</Box>
-
-				<Footer />
+				<Nav_bar />
 			</div>
 		</Box>
 	);
